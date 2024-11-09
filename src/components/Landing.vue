ab536efbf04af05f049223f4e37b1329f0adf6aa
Gautham Mallipeddi <gautham.mallipeddi8@gmail.com>
2024-11-09 19:43:08 +0530

    init:

---
+++ src/components/Landing.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <div class="w-full h-[calc(100vh)] flex gap-y-3 flex-col justify-center items-center">
+        <div class="text-5xl text-gray-50">
+            Gautham Mallipeddi
+        </div>
+        <div class="text-4xl text-gray-50">
+            Software Developer
+        </div>
+        <div class="text-2xl text-gray-50">
+            Full-stack Engineer | ML Engineer
+        </div>
+    </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Landing from '../components/Landing.vue'
+</script>
+
+<template>
+  <main>
+    <Landing />
+    something
+    something more
+  </main>
+</template>

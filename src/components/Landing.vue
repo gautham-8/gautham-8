34b3175df9ccd974089982d96372fee90e62bfb2
Gautham Mallipeddi <gautham.mallipeddi8@gmail.com>
2024-11-09 19:43:08 +0530

    init:

---
+++ src/components/Landing.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LeftSection from './LeftSection.vue';
+import RightSection from './RightSection.vue';
+</script>
+
+<template>
+    <div class="flex">
+        <LeftSection class="w-1/2" />
+        <RightSection class="w-1/2" />
+    </div>
+</template>
---
+++ src/components/LeftSection.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { computed } from 'vue';
+
+const SECTIONS = [
+    'ABOUT',
+    'WORK EXPERIENCE',
+    'EDUCATION',
+    'PROJECTS',
+    'SKILLS',
+    'RESUME [R]',
+];
+
+const active_section = computed(() => {
+    return SECTIONS[0];
+});
+</script>
+
+<template>
+    <div class="p-20">
+        <div class="text-4xl font-bold mb-2">
+            Gautham Mallipeddi
+        </div>
+        <div class="text-2xl font-light mb-4">
+            Software Engineer
+        </div>
+        <div class="text-md text-gray-300">
+            I develop full-stack applications and am currently exploring AI and ML technologies.
+        </div>
+        <div class="my-28">
+            <div
+                v-for="section in SECTIONS"
+                class="my-2"
+                :class="{
+                    'text-green-300': section === active_section,
+                    'text-gray-300': section !== active_section,
+                }"
+            >
+                {{ section }}
+            </div>
+        </div>
+    </div>
+</template>

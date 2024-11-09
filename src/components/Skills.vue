18ec5a1d7ff5f2483fd1614de36c4958964464f2
Gautham Mallipeddi <gautham.mallipeddi8@gmail.com>
2024-11-09 20:26:57 +0530

    wip:

---
+++ src/components/Skills.vue
@@ -0,0 +1,23 @@
+<script setup>
+import SkillIconsHtml from '../../assets/icons/SkillIconsHtml.vue';
+import SkillIconsCss from '../../assets/icons/SkillIconsCss.vue';
+import SkillIconsJavascript from '../../assets/icons/SkillIconsJavascript.vue';
+import SkillIconsVueLight from '../../assets/icons/SkillIconsVuejsLight.vue';
+import SkillIconsReactLight from '../../assets/icons/SkillIconsReactLight.vue';
+</script>
+
+<template>
+    <div class="flex flex-col items-center text-gray-50 mb-20">
+        <div class="text-4xl">
+            My skills
+        </div>
+        <div class="flex">
+            Web skills:
+            <SkillIconsHtml />
+            <SkillIconsCss />
+            <SkillIconsJavascript />
+            <SkillIconsVueLight />
+            <SkillIconsReactLight />
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Landing from '../components/Landing.vue'
+import Landing from '../components/Landing.vue';
+import Skills from '../components/Skills.vue';
 </script>
 
 <template>
   <main>
     <Landing />
-    something
-    something more
+    <Skills />
   </main>
 </template>
